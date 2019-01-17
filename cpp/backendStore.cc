29c24a26e745ed4291b002c4e49730825f8dd175
vagrant <vagrant@ubuntu1804.localdomain>
2019-01-17 13:19:17 -0800

    added a map to backendStore

--- cpp/backendStore.cc
+++ cpp/backendStore.cc
@@ -1,19 +1,24 @@
 #include <grpcpp/grpcpp.h>
 #include "backendStore.grpc.pb.h"
+#include <map>
+#include <string>
 
 class KeyValueStoreServiceImpl final : public chirp::KeyValueStore::Service{
-  grpc::Status put(grpc::ServerContext* context, const chirp::PutRequest* request, chirp::PutReply* reply) override {
-    // put whatever the request says
-    return grpc::Status::OK;
-  }
-  grpc::Status get(grpc::ServerContext* context, const chirp::GetRequest* request, chirp::GetReply* reply) {
-    // get whatever the request wants and set it as value in reply
-    return grpc::Status::OK;
-  }
-  grpc::Status deletekey(grpc::ServerContext* context, const chirp::DeleteRequest* request, chirp::DeleteReply* reply) override {
-    // delete what the request wants
-    return grpc::Status::OK;
-  }
+  public:
+    grpc::Status put(grpc::ServerContext* context, const chirp::PutRequest* request, chirp::PutReply* reply) override {
+      // put whatever the request says
+      return grpc::Status::OK;
+    }  
+    grpc::Status get(grpc::ServerContext* context, const chirp::GetRequest* request, chirp::GetReply* reply) {
+      // get whatever the request wants and set it as value in reply
+      return grpc::Status::OK;
+    }
+    grpc::Status deletekey(grpc::ServerContext* context, const chirp::DeleteRequest* request, chirp::DeleteReply* reply) override {
+      // delete what the request wants
+      return grpc::Status::OK;
+    }
+  private:
+    std::map<std::string, std::string> store; 
 };
 
 void RunServer(){
