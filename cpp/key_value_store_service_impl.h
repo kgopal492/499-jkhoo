df674741486a2791d80ead641e147a93f032495b
vagrant <vagrant@ubuntu1804.localdomain>
2019-01-20 09:32:39 -0800

    comment fixes

--- cpp/key_value_store_service_impl.h
+++ cpp/key_value_store_service_impl.h
@@ -4,7 +4,7 @@
 #include <grpcpp/grpcpp.h>
 #include "backendStore.grpc.pb.h"
 
-// class for logic and data behind the KeyValueStore
+// Class for logic and data behind the KeyValueStore
 class KeyValueStoreServiceImpl final : public chirp::KeyValueStore::Service {
  public:
   grpc::Status put(grpc::ServerContext* context, const chirp::PutRequest* request, chirp::PutReply* reply) override;
@@ -12,7 +12,6 @@ class KeyValueStoreServiceImpl final : public chirp::KeyValueStore::Service {
   grpc::Status deletekey(grpc::ServerContext* context, const chirp::DeleteRequest* request, chirp::DeleteReply* reply) override;
 
  private:
-  // map to hold the keys and values of the KeyValueStoreServiceImpl
-  // key is a string, value is a string
+  // Map to store the keys and values of the KeyValueStoreServiceImpl
   std::map<std::string, std::string> store_;
 };
