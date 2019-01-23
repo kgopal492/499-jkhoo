bd9d1a38154a63c68e04dac1e796a89f9ec0d046
vagrant <vagrant@ubuntu1804.localdomain>
2019-01-22 23:43:06 -0800

    service header file fixes

--- cpp/service_layer_service_impl.h
+++ cpp/service_layer_service_impl.h
@@ -8,19 +8,19 @@
 // The methods in this class are the implementation of the Service Layer Service from service.proto
 class ServiceLayerServiceImpl final : public chirp::ServiceLayer::Service {
  public:
-  grpc::Status registeruser(grpc::ServerContext* context, const chirp::RegisterRequest* request, chirp::RegisterReply* reply) override;
-  grpc::Status chirp(grpc::ServerContext* context, const chirp::ChirpRequest* request, chirp::ChirpReply* reply) override;
-  grpc::Status follow(grpc::ServerContext* context, const chirp::FollowRequest* request, chirp::FollowReply* reply) override;
-  grpc::Status read(grpc::ServerContext* context, const chirp::ReadRequest* request, chirp::ReadReply* reply) override;
-  grpc::Status monitor(grpc::ServerContext* context, const chirp::MonitorRequest* request, chirp::MonitorReply* reply);
   enum ActionEnum {
     kUserChirps = 0,
     kUserFollowing = 1,
     kChirpText = 2,
     kChirpReplies = 3,
   };
+  grpc::Status registeruser(grpc::ServerContext* context, const chirp::RegisterRequest* request, chirp::RegisterReply* reply) override;
+  grpc::Status chirp(grpc::ServerContext* context, const chirp::ChirpRequest* request, chirp::ChirpReply* reply) override;
+  grpc::Status follow(grpc::ServerContext* context, const chirp::FollowRequest* request, chirp::FollowReply* reply) override;
+  grpc::Status read(grpc::ServerContext* context, const chirp::ReadRequest* request, chirp::ReadReply* reply) override;
+  grpc::Status monitor(grpc::ServerContext* context, const chirp::MonitorRequest* request, chirp::MonitorReply* reply);
 
  private:
-  // curr_id_ keeps track of what the id of the next chirp should be
+  // keeps track of the id of the next chirp
   int curr_id_ = 0;
 };
