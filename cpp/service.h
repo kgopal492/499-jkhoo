23c8815ae32f7072495f6278f042649f4117fcf5
vagrant <vagrant@ubuntu1804.localdomain>
2019-01-23 21:47:56 -0800

    pr fixes

--- cpp/service.h
+++ cpp/service.h
@@ -8,17 +8,25 @@
 // The methods in this class are the implementation of the Service Layer Service from service.proto
 class ServiceLayer final {
  public:
+  // Enum with what to prepend keys with for adding values into the KeyValueStore
+  // To keep track of where different data is stored.
   enum ActionEnum {
     kUserChirps = 0,
     kUserFollowing = 1,
     kChirpText = 2,
     kChirpReplies = 3,
   };
-  void registeruser(std::string username);
-  chirp::Chirp chirp(std::string username, std::string text, std::string parent_id);
-  void follow(std::string username, std::string to_follow);
-  chirp::Chirp read(std::string chirp_id);
-  chirp::Chirp monitor(std::string username);
+
+  // Communicates with KeyValueStoreServiceImpl to register username
+  void registeruser(const std::string& username);
+  // Communicates with KeyValueStoreServiceImpl to add a chirp
+  chirp::Chirp chirp(const std::string& username, const std::string& text, const std::string& parent_id);
+  // Communicates with KeyValueStoreServiceImpl to add to_follow to username's list of people following
+  void follow(const std::string& username, const std::string& to_follow);
+  // Communicates with KeyValueStoreServiceImpl to read from chirp_id
+  chirp::Chirp read(const std::string& chirp_id);
+  // Communicates with KeyValueStoreServiceImpl to stream chirps from the users username is following
+  chirp::Chirp monitor(const std::string& username);
 
  private:
   // keeps track of the id of the next chirp
